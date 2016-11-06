5cbb14df8f0d3ded3858bcffb10e2db1568065ce
Alex Luckett <alexluckett@users.noreply.github.com>
2016-11-06 11:06:57 +0000

    Removed console logs, formatted code

--- maps.js
+++ maps.js
@@ -20,44 +20,44 @@ function initMap() {
         calculateAndDisplayRoute(directionsService, directionsDisplay);
     };
 
-	
-    
+
+
     //document.getElementById('start').addEventListener('change', onChangeHandler);
     //document.getElementById('end').addEventListener('change', onChangeHandler);
     document.getElementById("clicky").addEventListener('click', onChangeHandler);
 }
 
 function toggleHeatmap() {
-        heatmap.setMap(heatmap.getMap() ? null : map);
-      }
+    heatmap.setMap(heatmap.getMap() ? null : map);
+}
 
-      function changeGradient() {
-        var gradient = [
-          'rgba(0, 255, 255, 0)',
-          'rgba(0, 255, 255, 1)',
-          'rgba(0, 191, 255, 1)',
-          'rgba(0, 127, 255, 1)',
-          'rgba(0, 63, 255, 1)',
-          'rgba(0, 0, 255, 1)',
-          'rgba(0, 0, 223, 1)',
-          'rgba(0, 0, 191, 1)',
-          'rgba(0, 0, 159, 1)',
-          'rgba(0, 0, 127, 1)',
-          'rgba(63, 0, 91, 1)',
-          'rgba(127, 0, 63, 1)',
-          'rgba(191, 0, 31, 1)',
-          'rgba(255, 0, 0, 1)'
-        ]
-        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-      }
+function changeGradient() {
+    var gradient = [
+        'rgba(0, 255, 255, 0)',
+        'rgba(0, 255, 255, 1)',
+        'rgba(0, 191, 255, 1)',
+        'rgba(0, 127, 255, 1)',
+        'rgba(0, 63, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 223, 1)',
+        'rgba(0, 0, 191, 1)',
+        'rgba(0, 0, 159, 1)',
+        'rgba(0, 0, 127, 1)',
+        'rgba(63, 0, 91, 1)',
+        'rgba(127, 0, 63, 1)',
+        'rgba(191, 0, 31, 1)',
+        'rgba(255, 0, 0, 1)'
+    ]
+    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
 
-      function changeRadius() {
-        heatmap.set('radius', heatmap.get('radius') ? null : 20);
-      }
+function changeRadius() {
+    heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
 
-      function changeOpacity() {
-        heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-      }
+function changeOpacity() {
+    heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
 
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
@@ -85,24 +85,23 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
 }
 
 function displayStats(stats) {
-	var result = new Array();
-   	 for (var key in stats) {
+    var result = new Array();
+    for (var key in stats) {
         // skip loop if the property is from prototype
-        	if (!stats.hasOwnProperty(key)) continue;
+        if (!stats.hasOwnProperty(key))
+            continue;
 
-        	var obj = stats[key];
-		console.log(obj);
-		result.push(
-			{
-			id : parseFloat(obj.lat),
-			location: new google.maps.LatLng(parseFloat(obj.lat), parseFloat(obj.lng)),
-			weight: 0.5
-			});
-	}
-	console.log(result);
-	heatmap = new google.maps.visualization.HeatmapLayer({
-          data: result,
-          map: map
-        });
-	
+        var obj = stats[key];
+        result.push(
+                {
+                    id: parseFloat(obj.lat),
+                    location: new google.maps.LatLng(parseFloat(obj.lat), parseFloat(obj.lng)),
+                    weight: 0.5
+                });
+    }
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        data: result,
+        map: map
+    });
+
 }
